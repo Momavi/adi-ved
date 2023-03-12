f9f42de2715fbb365fccb40ca9f45ceb84cd0fd3
Erbis Vladislav <cool.ru1960@gmail.com>
2023-03-12 10:26:10 +0300

    [ADD] Разметил folder structure

---
+++ src/pages/Home/HomePage.tsx
@@ -0,0 +1,14 @@
+import "./home.scss";
+
+function HomePage() {
+  return (
+    <div>
+      <div className="w-2/3">
+        <p className="logo">“АДИ-ВЕД”</p>
+      </div>
+      <div className="w-1/3"></div>
+    </div>
+  );
+}
+
+export default HomePage;
